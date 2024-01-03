d9f1d247ddc7c2374ef320aab69997f7da2cda7c
Ali Samir <alisamir.eng@gmail.com>
2024-01-03 16:31:31 +0200

    project structure

--- task-manager/app.js
+++ task-manager/app.js
@@ -1,18 +1,31 @@
 const express = require("express");
-const app = express();
 const tasks = require("./routes/tasks.js");
+const connectDB = require("./db/connect.js");
+require("dotenv").config();
+
+const notFound = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
+const app = express();
 
 // Middleware
+app.use(express.static("./public"));
 app.use(express.json());
 
 // Routes
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-
 app.use("/api/v1/tasks", tasks);
 
-const PORT = 8000;
-app.listen(PORT, () => {
-  console.log(`Server Is Running On Port: ${PORT}`);
-});
+app.use(notFound);
+app.use(errorHandlerMiddleware);
+
+const PORT = process.env.PORT || 8000;
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGODB_URI);
+    app.listen(PORT, () => console.log(`Server Is Running On PORT: ${PORT}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
